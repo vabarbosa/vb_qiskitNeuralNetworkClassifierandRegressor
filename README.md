71816fe9d25e2dea8be35bbe572209e154e89b2d
alexviatela <80417070+alexviatela@users.noreply.github.com>
2023-03-21 23:15:04 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -34,6 +34,8 @@ from qiskit_machine_learning.neural_networks import SamplerQNN, EstimatorQNN
 ```
 
 ### Modelo de Clasificación
+Implementa un clasificador básico de redes neuronales cuánticas. Se implementan métodos compatibles con Scikit-Learn para clasificar y extender ClassifierMixin. 
+
 Se realiza la preparación de un conjunto de datos preliminar para entrenar las redes neuronales.
 ![](img/2-ClasificacionDataset.png)
 > Preparación del DataSet.
@@ -42,15 +44,25 @@ Se realiza la preparación de un conjunto de datos preliminar para entrenar las 
 EstimatorQNN una red neuronal que toma un circuito cuántico parametrizado con parámetros designados para datos de entrada y/o pesos, observables opcionales y genera sus valores esperados.
 
 Inicialmente se espera que devuelva una salida unidimensional
-![]()
+![](img/3.png)
 
-Una vez se ha crea la red neuronal cuántica, se crea la función callback_graph esta llamará para cada iteración del optimizador y se le pasarán dos parámetros: los pesos actuales y el valor de la función objetivo en esos pesos. 
+Una vez se ha crea la red neuronal cuántica, se crea la función callback_graph esta llamará para cada iteración del optimizador y se le pasarán dos parámetros: los pesos actuales y el valor de la función objetivo en esos pesos.
 
 Se realiza el entrenamiento del modelo y las predicciones correspondientes, una vez se ha realizado el entrenamiento se pueden explorar los pesos de la red neuronal.
-![]()
-![]()
+![](img/4.png)
+![](img/5.png)
+![](img/5-1.png)
 
 #### Clasificación con SamplerQNN
 SamplerQNN es una red neuronal que toma un circuito cuántico parametrizado con parámetros designados para datos de entrada y/o pesos y traduce las cuasi-probabilidades estimadas por el Sampler primitivo en clases predichas.
+![](img/6.png)
 
-Los pasos realizados para 
+Los pasos realizados para la clasificación son los mismos para EstimatorQNN.
+![](img/7.png)
+![](img/8.png)
+![](img/9.png)
+
+#### Clasificación con VQC (Variational Quantum Classifier)
+El VQC es una variante de una Red Neuronal de Clasificación con un SamplerQNN, se aplica un mapeo de paridad desde la cadena de bits hasta la clasificación, lo que da como resultado un vector de probabilidad, que se interpreta como un resultado codificado en caliente.
+![](img/10.png)
+![](img/11.png)
--- main.py
+++ main.py
@@ -30,6 +30,71 @@ for x, y_target in zip(X, y):
 plt.plot([-1, 1], [1, -1], "--", color="black")
 plt.show()
 
+########## EstimatorQNN ##########
+
+# construct QNN
+qc = QuantumCircuit(2)
+feature_map = ZZFeatureMap(2)
+ansatz = RealAmplitudes(2)
+qc.compose(feature_map, inplace=True)
+qc.compose(ansatz, inplace=True)
+qc.draw(output="mpl")
+
+estimator_qnn = EstimatorQNN(
+    circuit=qc, input_params=feature_map.parameters, weight_params=ansatz.parameters
+)
+
+estimator_qnn.forward(X[0, :], algorithm_globals.random.random(estimator_qnn.num_weights))
+
+
+def callback_graph(weights, obj_func_eval):
+    clear_output(wait=True)
+    objective_func_vals.append(obj_func_eval)
+    plt.title("Objective function value against iteration")
+    plt.xlabel("Iteration")
+    plt.ylabel("Objective function value")
+    plt.plot(range(len(objective_func_vals)), objective_func_vals)
+    plt.show()
+
+# construct neural network classifier
+estimator_classifier = NeuralNetworkClassifier(
+    estimator_qnn, optimizer=COBYLA(maxiter=60), callback=callback_graph
+)
+
+# create empty array for callback to store evaluations of the objective function
+objective_func_vals = []
+plt.rcParams["figure.figsize"] = (12, 6)
+
+# fit classifier to data
+estimator_classifier.fit(X, y)
+
+# return to default figsize
+plt.rcParams["figure.figsize"] = (6, 4)
+
+# score classifier
+estimator_classifier.score(X, y)
+
+# evaluate data points
+y_predict = estimator_classifier.predict(X)
+
+# plot results
+# red == wrongly classified
+for x, y_target, y_p in zip(X, y, y_predict):
+    if y_target == 1:
+        plt.plot(x[0], x[1], "bo")
+    else:
+        plt.plot(x[0], x[1], "go")
+    if y_target != y_p:
+        plt.scatter(x[0], x[1], s=200, facecolors="none", edgecolors="r", linewidths=2)
+plt.plot([-1, 1], [1, -1], "--", color="black")
+plt.show()
+
+print('estimator_classifier.weights')
+print(estimator_classifier.weights)
+
+
+########## SamplerQNN ##########
+
 # construct feature map
 feature_map = ZZFeatureMap(num_inputs)
 
@@ -76,3 +141,64 @@ plt.rcParams["figure.figsize"] = (6, 4)
 # score classifier
 sampler_classifier.score(X, y01)
 
+# evaluate data points
+y_predict = sampler_classifier.predict(X)
+
+# plot results
+# red == wrongly classified
+for x, y_target, y_p in zip(X, y01, y_predict):
+    if y_target == 1:
+        plt.plot(x[0], x[1], "bo")
+    else:
+        plt.plot(x[0], x[1], "go")
+    if y_target != y_p:
+        plt.scatter(x[0], x[1], s=200, facecolors="none", edgecolors="r", linewidths=2)
+plt.plot([-1, 1], [1, -1], "--", color="black")
+plt.show()
+
+print('sampler_classifier.weights')
+print(sampler_classifier.weights)
+
+
+########## VQC ##########
+
+# construct feature map, ansatz, and optimizer
+feature_map = ZZFeatureMap(num_inputs)
+ansatz = RealAmplitudes(num_inputs, reps=1)
+
+# construct variational quantum classifier
+vqc = VQC(
+    feature_map=feature_map,
+    ansatz=ansatz,
+    loss="cross_entropy",
+    optimizer=COBYLA(maxiter=30),
+    callback=callback_graph,
+)
+
+# create empty array for callback to store evaluations of the objective function
+objective_func_vals = []
+plt.rcParams["figure.figsize"] = (12, 6)
+
+# fit classifier to data
+vqc.fit(X, y_one_hot)
+
+# return to default figsize
+plt.rcParams["figure.figsize"] = (6, 4)
+
+# score classifier
+vqc.score(X, y_one_hot)
+
+# evaluate data points
+y_predict = vqc.predict(X)
+
+# plot results
+# red == wrongly classified
+for x, y_target, y_p in zip(X, y_one_hot, y_predict):
+    if y_target[0] == 1:
+        plt.plot(x[0], x[1], "bo")
+    else:
+        plt.plot(x[0], x[1], "go")
+    if not np.all(y_target == y_p):
+        plt.scatter(x[0], x[1], s=200, facecolors="none", edgecolors="r", linewidths=2)
+plt.plot([-1, 1], [1, -1], "--", color="black")
+plt.show()
